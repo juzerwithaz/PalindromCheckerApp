ccae330b6a0fa9218c480fc3864d0d2c2d6f812a
juzerwithaz <juzersm7@gmail.com>
2026-02-26 09:25:18 +0530

    Juzer UC1

---
+++ App/src/PalindromeCheckerApp.java
@@ -0,0 +1,16 @@
+/**
+ * ===================================================
+ * MAIN CLASS - UseCase1PalindromeApp
+ * ===================================================
+ *
+ * Use Case 1: Application Entry & Welcome Message
+ *
+ * Description:
+ * This class represents the entry point of the
+ * palindrome checker Management System.
+**/
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Palindrome app management system");
+    }
+}
